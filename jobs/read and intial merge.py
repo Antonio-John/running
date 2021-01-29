7247644c2ae7c757218a634f2e18303a2371020d
pyantonio-coder <antonio.felton@virginmedia.com>
2021-01-29 22:40:18 +0000

    Initial reading in data and merging with dates

---
+++ jobs/read and intial merge.py
@@ -0,0 +1,26 @@
+from tools import read_file, get_dates_df, merge
+from configparser import ConfigParser
+from datetime import datetime
+
+def read_merge_with_dates(config, now_date):
+    """
+    :param config:
+    :param now_date:
+    :return:
+    """
+    raw_df = read_file(config)
+    date_df = get_dates_df(now_date)
+
+    print(raw_df.columns)
+    merged_df = merge(raw_df, date_df)
+
+    merged_df.to_csv(config.get("merged","running_merged"))
+
+    return merged_df
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read('C:\Running\config\config.properties')
+    now = datetime.today().strftime("%m/%d/%Y")
+    read_merge_with_dates(config, now)
--- src/tools.py
+++ src/tools.py
@@ -1,21 +1,32 @@
-from configparser import ConfigParser
 import pandas as pd
 
-def read(config):
+def read_file(config):
     """
     :param config file that is used in the project
     :return: the latest running data with pandas
+    reads in running files and return dataframe
     """
-    file_path = config.get("io", "running")
+    file_path = config.get("raw", "running")
     df = pd.read_excel(file_path,
-                       usecols=9)
+                       usecols=list(range(8)))
 
     return df
 
+def get_dates_df(end_date):
+    """
+    :param end date is the
+    :return: df_dates a dataframe with all dates
+    from when running started
+    """
+    df_dates = pd.DataFrame(pd.date_range(start='10/12/2019',
+                                       end=end_date),
+                                       columns = ["Date"])
+
+    return df_dates
 
 
+def merge(raw_df, df_date):
 
-if __name__ == "__main__":
-    config = ConfigParser()
-    config.read('C:\Running\config\config.properties')
-    read(config)
+    merge = df_date.merge(raw_df, how="left", on="Date")
+
+    return merge
