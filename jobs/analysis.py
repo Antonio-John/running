aedd8935c14fa7e6d9c958f8d5cfb6a34267181b
pyantonio-coder <antonio.felton@virginmedia.com>
2021-01-30 18:16:40 +0000

    Work on analysis

--- jobs/analysis.py
+++ jobs/analysis.py
@@ -6,15 +6,27 @@
 from configparser import ConfigParser
 from datetime import datetime
 import pandas as pd
+from analysis_tools import month_barchart
+import matplotlib.pyplot as plt
+import os
 
 def analysis(config, now):
 
+    # reads in file
     df = pd.read_csv(config.get("processed","running_processed"))
 
-    print(df)
+    # sets up directory
+    directory = config.get("analysis", "folder_analysis") + now
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # monthly barchart
+    monthly_barchart = month_barchart(df)
+    plt.savefig(directory+"\monthly_barchart.png")
+
 
 if __name__ == "__main__":
     config = ConfigParser()
     config.read('C:\Running\config\config.properties')
-    now = datetime.today().strftime("%m/%d/%Y")
+    now = datetime.today().strftime("%Y%m%d")
     analysis(config, now)
--- src/analysis_tools.py
+++ src/analysis_tools.py
@@ -1,5 +1,19 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import pandas as pd
 
+
+
+def month_barchart(df):
+
+    df["month_year"] = df["month_year"].astype(str)
+    df_group_month = df.groupby(["month_year"]).sum()
+
+    monthly_bar_time = plt.bar(df_group_month.index, df_group_month["distance"], align='center', alpha=0.5)
+    plt.xticks(rotation='vertical')
+    plt.rc('xtick', labelsize=10)
+    plt.rc('ytick', labelsize=6)
+
+    return monthly_bar_time
 
 
 
