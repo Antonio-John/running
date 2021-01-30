3d184abbf289c8a37100b1bfee6af433e4563514
pyantonio-coder <antonio.felton@virginmedia.com>
2021-01-30 19:13:05 +0000

    Set Up Py

---
+++ src/Setup.py
@@ -0,0 +1,25 @@
+from setuptools import (setup,
+                        find_packages)
+
+setup(name='runnings',
+      version='0.0.1',
+      author='Antonio Felton',
+      packages=find_packages(
+            include=["src", "src.*",]),
+    install_requires=["pandas",
+                      "Pillow",
+                      "cycler",
+                      "et-xmlfile",
+                      "jdcal",
+                      "kiwisolver",
+                      "matplotlib",
+                      "numpy",
+                      "openpyxl",
+                      "pandas",
+                      "pip",
+                      "pyparsing",
+                      "python-dateutil",
+                      "pytz",
+                      "setuptools",
+                      "six",
+                      "xlrd"])
