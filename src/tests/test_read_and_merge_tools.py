9c250910adb12c3d382de5c65bffbc6de5b47729
pyantonio-coder <antonio.felton@virginmedia.com>
2021-05-10 14:38:45 +0100

    adding tests

---
+++ src/tests/test_read_and_merge_tools.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from src.read_and_merge_tools import get_dates_df
+from datetime import datetime
+
+def test_get_dates_df():
+
+    output_df=pd.DataFrame({'date':[pd.to_datetime('10/12/2019'),
+                                    pd.to_datetime('10/13/2019'),
+                                    pd.to_datetime('10/14/2019')]})
+
+    result=get_dates_df("10/14/2019")
+
+    pd.testing.assert_frame_equal(output_df,result)
+
+
+
+
+
+
