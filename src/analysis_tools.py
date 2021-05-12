2865b7971ee6c813f07e6ec3f0b67a012ec20884
pyantonio-coder <antonio.felton@virginmedia.com>
2021-05-12 18:57:35 +0100

    Adding docs to the analysis tools module

--- src/analysis_tools.py
+++ src/analysis_tools.py
@@ -1,64 +1,73 @@
+"""
+This is the analysis tools module. This creates all the charts associated with analysis
+Functions
+*barchart
+*rolling_average
+*histo
+*line_plot
+"""
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
-def barchart(df, type, time_period, category):
+def barchart(df: pd.core.frame.DataFrame, by_stat: str, time_period: str, category: str):
     """
-    df is the dataframe and type is count or sum per month
+    :param df: Pandas dataframe which creates the barchart
+    :param by_stat: By which stat e.g by sum/count/average
+    :param time_period: Over which time period should the barhcart be (i.e X axis)
+    :param category: Units of Y axis
+    :return:
     """
-
-    if time_period=="week":
-        df["week"] = df["week"].astype(int)
+    if time_period == "week":
+        df["week"] = df["week"].asby_stat(int)
     else:
-        df[time_period] = df[time_period].astype(str)
-
+        df[time_period] = df[time_period].asby_stat(str)
 
     plt.figure()
 
-    if type=="sum":
+    if by_stat == "sum":
         df_group = df.groupby([time_period]).sum()
-    elif type=="count":
+    elif by_stat == "count":
         df_group = df.groupby([time_period]).count()
-    elif type=="average":
+    elif by_stat == "average":
         df_group = df.groupby([time_period]).mean()
 
     if time_period == "week":
-        df_group=df_group.sort_index()
+        df_group = df_group.sort_index()
 
-    bar_time = plt.bar(df_group.index, df_group[category], align='center', alpha=0.5)
+    plt.bar(df_group.index, df_group[category], align='center', alpha=0.5)
     plt.xticks(rotation='vertical')
     plt.rc('xtick', labelsize=10)
     plt.rc('ytick', labelsize=6)
     plt.xlabel(time_period)
     plt.ylabel("Totals")
-    plt.title(type+' Per '+time_period)
+    plt.title(by_stat + ' Per ' + time_period)
     plt.tight_layout()
 
-    return df_group
-
-def rolling_average(df, t):
+def rolling_average(df:pd.core.frame.DataFrame, time_period):
     """
-    
+    :param df: Pandas dataframe used to create rolling average chart
+    :param time_period: Time period used for the rolling average
+    :return:
     """
     plt.figure()
     df['distance'] = df['distance'].fillna(0)
-    df['rolling_average_'+str(t)+'_day'] = df.iloc[:, 4].rolling(window=t).sum()
-    rolling_avg_30 = plt.plot(df["date"], df['rolling_average_'+str(t)+'_day'])
+    df['rolling_average_'+str(time_period)+'_day'] = df.iloc[:, 4].rolling(window=time_period).sum()
+    plt.plot(df["date"], df['rolling_average_'+str(time_period)+'_day'])
 
-def histo(df, type):
+def histo(df:pd.core.frame.DataFrame, by_stat:str):
     """
-    plot histogram
+    :param df: Pandas dataframe used to create the histograms
+    :param by_stat: by which stat e.g average, count
     """
     plt.figure()
-    plt.hist(df[type], bins=20, align="mid")
+    plt.hist(df[by_stat], bins=20, align="mid")
 
-
-
-def line_plot(df, type):
-
+def line_plot(df:pd.core.frame.DataFrame, by_stat:str):
+    """
+    :param df: Pandas dataframe used to create the line plot
+    :param by_stat: by which stat e.g average, count
+    """
     plt.figure()
-    plt.plot(df.index, df[type])
+    plt.plot(df.index, df[by_stat])
     plt.tight_layout()
-
-
